e752952319103030002f458923ac056cd4c8529a
maaaaquiiii <maqui.plaza02@gmail.com>
2026-03-15 21:31:02 +0100

    chore: project initial structure

---
+++ src/main/java/com/aureus/AureusApplication.java
@@ -0,0 +1,11 @@
+package com.aureus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AureusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AureusApplication.class, args);
+    }
+}
